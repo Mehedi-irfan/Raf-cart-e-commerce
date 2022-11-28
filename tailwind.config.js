b469a24b71050a1e2a130020f41d7ebd1ff18333
Mehedi-irfan <mehediirfan99@gmal.com>
2022-11-28 14:12:26 +0600

    top header section added with responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+      colors: {
+        primary: "#fd3d57",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
