6110df1c1ab80d3b7ba05b2c912b33cffe6d0c9c
Mehedi-irfan <mehediirfan99@gmal.com>
2022-12-01 09:43:23 +0600

    slider section added

---
+++ JS/Script.js
@@ -0,0 +1,13 @@
+// swiper js slider script
+var swiper = new Swiper(".home-slider", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
