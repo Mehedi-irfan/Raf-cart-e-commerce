2cf286145493ade3cfd6a122bc1597fba939bdae
Mehedi-irfan <mehediirfan99@gmal.com>
2022-12-02 16:46:54 +0600

    new arrival section added with responsive

--- JS/Script.js
+++ JS/Script.js
@@ -9,3 +9,41 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// New arrival slider
+var swiper = new Swiper(".new_arrival", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  loopFillGroupWithBlank: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1136: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
